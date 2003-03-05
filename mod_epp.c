1e14338c83d1f1c05b55114d6f6801a880be6b0b
otmar <otmar>
2003-03-05 15:35:11 +0000

    Licence

--- mod_epp.c
+++ mod_epp.c
@@ -1,14 +1,50 @@
-
 /*
- * This code is copyright NIC.at.
+ * Copyright (c) 2002 NIC.at Internet Verwaltungs- und 
+ * Betriebsgesellschaft m. b. H. All rights reserved.
  *
  * Written by Otmar Lendl <lendl@nic.at>
  *
- * We intend to release this code under the Apache licence.
- * A more formal copyright notice will appear in future releases,
- * this is just a quick placeholder to get the code out at all.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
  *
- * /ol/2k2/11/06/
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *    "This product includes software developed by the
+ *    NIC.at Internet Verwaltungs- und Betriebsgesellschaft m. b. H."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "mod_epp" and "NIC.at" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact lendl@nic.at
+ *
+ * 5. Products derived from this software may not be called "mod_epp",
+ *    nor may "mod_epp" appear in their name, without prior written
+ *    permission of NIC.at.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL NIC.AT INTERNET VERWALTUNGS- UND
+ * BETRIEBSGESELLSCHAFT M.B.H. OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  *
  */
 
@@ -35,8 +71,7 @@
 
 module AP_MODULE_DECLARE_DATA epp_module;
 
-
-/* from mod_jabber */
+/* two simple xml helpers from mod_jabber */
 
 char *get_attr(apr_xml_attr *attr, const char *name) 
 {
@@ -61,8 +96,9 @@ void xml_firstcdata_strncat(char *dest, size_t dstsize, apr_xml_elem *elem)
 {
 apr_text *t;
 
-dstsize--;
+dest[dstsize] = 0;
 dest[0] = 0;
+dstsize--;
 
 for (t = elem->first_cdata.first; t; t = t->next) 
 	{
